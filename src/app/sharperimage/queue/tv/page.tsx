664cda8a05cc54a1f504efece9f1da087f6213e8
joshwagner1798-source <joshwagner1798@gmail.com>
2026-02-24 23:53:58 -0500

    Make clean TV route use real TV display

--- src/app/sharperimage/queue/tv/page.tsx
+++ src/app/sharperimage/queue/tv/page.tsx
@@ -1,5 +1,3 @@
-import { TVDisplay } from "@/app/tv/TVDisplay";
+import TVDisplayPage from '@/app/tv-display/page'
 
-export default function Page() {
-  return <TVDisplay shopId="70467794-c7ce-47f2-8c62-bcb5bb19e31e" />;
-}
+export default TVDisplayPage
